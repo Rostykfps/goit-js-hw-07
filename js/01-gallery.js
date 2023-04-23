e7a0caf1edea37cb4f3c9c590872347a7db792cd
Rostykfps <rostikfps@gmail.com>
2023-04-23 20:12:05 +0300

    Changed 02-lightbox.js, 02-lightbox.html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from 'basiclightbox';
 // Change code below this line
+const galleryListRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryListRef.innerHTML = createGalleryListMarkUp(galleryItems);
+galleryListRef.addEventListener('click', onImageGalleryClick);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+function createGalleryListMarkUp(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+// Реалізація делегування на ul.gallery і отримання url великого зображення.
+function onImageGalleryClick(event) {
+  // Заборона перенаправлення за замовчуванням
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const originalImage = event.target.dataset.source;
+
+  openModalWindow(originalImage);
+}
+
+// Відкриття модального вікна по кліку на елементі галереї.
+
+function openModalWindow(link) {
+  const instance = basicLightbox.create(`
+    <img src="${link}">
+`);
+  instance.show();
+
+  window.addEventListener('keydown', closeModalWindow);
+
+  // Pакриття модального вікна після натискання клавіші Escape.
+  // Прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
+  function closeModalWindow(event) {
+    if (event.code === 'Escape') {
+      instance.close(() => window.removeEventListener(event, closeModalWindow));
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryListRef = document.querySelector('.gallery');
+
+galleryListRef.innerHTML = createGalleryListMarkUp(galleryItems);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+function createGalleryListMarkUp(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+
+// Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery
+var lightbox = new SimpleLightbox('.gallery a', {
+  // Додаванная відображення підписів до зображень з атрибута alt.
+  // Налаштування щоб підпис був знизу і з'являвся через 250 мілісекунд після відкриття зображення.
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
